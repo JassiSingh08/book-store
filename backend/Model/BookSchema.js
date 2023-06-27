e008295fc10a6856ed821952dc14d97c10109ce0
JassiSingh08 <singhjassi693@gmail.com>
2023-06-27 14:20:19 +0530

    First Commit

---
+++ backend/Model/BookSchema.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const {Schema} = mongoose
+
+const BookSchema = new Schema({
+    name:{
+        type: String,
+        required : true
+    },
+    author:{
+        type: String,
+        required : true
+    },
+    description:{
+        type: String,
+        required : true
+    },
+    price:{
+        type: Number,
+        required : true
+    },
+    available:{
+        type: Boolean,
+        required : true
+    },
+})
+
+const Book = mongoose.model("Book", BookSchema)
+module.exports = Book;
---
+++ backend/Routes/BookRoutes.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const router = express.Router();
+const Book = require("../Model/BookSchema");
+
+router.get("/", async(req , res) => {
+    try {
+        let books = await Book.find()
+        if(!books){
+            return res.status(404).json("NO BOOKS FOUND")
+        }
+        return res.status(200).json({books})
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+router.post("/", async(req, res)=>{
+    try {
+       const {name, description , author, price, available } = req.body;
+        let Books = new Book({
+            name, description, author, price, available
+        })
+        await Books.save()
+        if(!Books){
+            return res.status(400).json({message: "Error in adding Book"})
+        }
+        return res.status(200).json({message: "New Book Added Successfully", Books})
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+router.put("/:id", async(req, res)=>{
+    try {
+        const {name, description , author, price, available } = req.body;
+    
+        let book = await Book.findByIdAndUpdate(req.params.id, {
+            name, description, author, price, available
+        })
+        if(!book){
+            return res.status(500).json({message: "Server error"})
+        }
+        return res.status(200).json({message:"Book Updated SuccessFully", book})
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+router.delete("/:id", async(req, res)=>{
+    try {
+        let book = await Book.findByIdAndDelete(req.params.id)
+        if(!book){
+            return res.status(500).json({message: "Server error"})
+        }
+        return res.status(200).json({message:"Book Deleted SuccessFully"})
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+module.exports = router;
